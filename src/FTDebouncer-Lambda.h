68c6b7bf28749e8d71d5cb7d28dceb0cd1592caf
Sebastian Hunkeler <s.hunkeler@arduino.cc>
2020-05-03 12:22:23 +0200

    Add include guard for lambda header

--- src/FTDebouncer-Lambda.h
+++ src/FTDebouncer-Lambda.h
@@ -1,5 +1,10 @@
+#ifndef FTDEBOUNCER_LAMBDA_H
+#define FTDEBOUNCER_LAMBDA_H
+
 #include "FTDebouncer.h"
 
 //Dummy function implementations to satisfy linker when using lambdas
 void onPinActivated(int pinNumber){}
 void onPinDeactivated(int pinNumber){}
+
+#endif
